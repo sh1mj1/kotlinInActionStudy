c95f5733801b6d1d8c93ff6838083e13621a6f5b
sh1mj1 <7wlgns@gmail.com>
2024-01-03 14:36:57 +0900

    chap(3): 확장 프로퍼티

--- src/main/kotlin/inaction/chap3/strings/StringUtil.kt
+++ src/main/kotlin/inaction/chap3/strings/StringUtil.kt
@@ -1,3 +1,10 @@
 package inaction.chap3.strings
 
 fun String.lastChar(): Char = this[this.length - 1]
+
+val String.lastChar: Char
+    get() = this[this.length - 1]
+
+var StringBuilder.lastChar: Char
+    get() = this[this.length - 1]
+    set(value) = this.setCharAt(length - 1, value)
