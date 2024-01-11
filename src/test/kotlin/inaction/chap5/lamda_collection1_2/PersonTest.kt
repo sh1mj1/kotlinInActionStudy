07ae9cbf7f6af7893cc02e956bbf609ad9896bae
sh1mj1 <7wlgns@gmail.com>
2024-01-11 15:18:34 +0900

    chap(5): 5.1.2 람다와 컬렉션

---
+++ src/test/kotlin/inaction/chap5/lamda_collection1_2/PersonTest.kt
@@ -0,0 +1,24 @@
+package inaction.chap5.lamda_collection1_2
+
+import org.junit.jupiter.api.Test
+
+class PersonTest {
+    @Test
+    fun testFindTheOldest1() {
+        val people = listOf(Person("Alice", 29), Person("Bob", 31))
+        assert(findTheOldest1(people) == Person("Bob", 31))
+    }
+
+    @Test
+    fun testFindTheOldest2() {
+        val people = listOf(Person("Alice", 29), Person("Bob", 31))
+        assert(findTheOldest2(people) == Person("Bob", 31))
+    }
+
+    @Test
+    fun testFindTheOldest3() {
+        val people = listOf(Person("Alice", 29), Person("Bob", 29))
+        assert(findTheOldest3(people) == Person("Alice", 29))
+    }
+
+}
