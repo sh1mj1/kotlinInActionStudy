ad51a81cabead6860dfb3fa6ffe6b9b31db3ab60
sh1mj1 <7wlgns@gmail.com>
2024-01-26 16:30:42 +0900

    item(9): use 을 사용해서 리소스를 닫아라

---
+++ src/main/kotlin/effective/item9/UseMethodUse.kt
@@ -0,0 +1,33 @@
+package effective.item9
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+fun countCharactersInFile(path: String): Int {
+    val reader = BufferedReader(FileReader(path))
+    try {
+        return reader.lineSequence().sumOf { it.length }
+    } finally {
+        reader.close()
+    }
+}
+
+fun countCharactersInFile2(path: String): Int {
+    val reader = BufferedReader(FileReader(path))
+    reader.use {
+        return reader.lineSequence().sumOf { it.length }
+    }
+}
+
+fun countCharactersInFile3(path: String): Int {
+    BufferedReader(FileReader(path)).use { reader ->
+        return reader.lineSequence().sumOf { it.length }
+    }
+}
+
+fun countCharactersInFile4(path: String): Int {
+    File(path).useLines { lines ->
+        return lines.sumOf { it.length }
+    }
+}
