c2dcab91bc8782bf44b365c3d1d52c69fe052778
sh1mj1 <7wlgns@gmail.com>
2024-01-31 13:40:45 +0900

    chap(9): 1.2 제네릭 클래스 선언

---
+++ src/main/kotlin/inaction/chap9/genericTypeParameter1/GenericClassDeclaration2.kt
@@ -0,0 +1,95 @@
+package inaction.chap9.genericTypeParameter1
+
+/*
+
+class StringList: List<String> { // 타입 인자를 String 을 지정해 List 를 구현
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun get(index: Int): String = TODO("Not yet implemented") // String 을 사용
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun iterator(): Iterator<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun listIterator(): ListIterator<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun listIterator(index: Int): ListIterator<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun subList(fromIndex: Int, toIndex: Int): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun lastIndexOf(element: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun indexOf(element: String): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun containsAll(elements: Collection<String>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(element: String): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
+
+class ArrayList<T>: List<T>{ // // ArrayList 의 제네릭 타입 인자 T 를 List 의 타입 인자로 넘긴다.
+    override val size: Int
+        get() = TODO("Not yet implemented")
+
+    override fun get(index: Int): T {
+        TODO("Not yet implemented")
+    }
+
+    override fun isEmpty(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun iterator(): Iterator<T> {
+        TODO("Not yet implemented")
+    }
+
+    override fun listIterator(): ListIterator<T> {
+        TODO("Not yet implemented")
+    }
+
+    override fun listIterator(index: Int): ListIterator<T> {
+        TODO("Not yet implemented")
+    }
+
+    override fun subList(fromIndex: Int, toIndex: Int): List<T> {
+        TODO("Not yet implemented")
+    }
+
+    override fun lastIndexOf(element: T): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun indexOf(element: T): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun containsAll(elements: Collection<T>): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun contains(element: T): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
+
+*/
